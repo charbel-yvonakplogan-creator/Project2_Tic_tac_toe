8b1bd9bebb50adf53c17401f4b2fd8d8da2c8036
Charbelakplogan2002 <acharbel02@gmail.com>
2025-10-17 12:17:02 +0000

    Création de la grille de jeu avec initialisation des espaces vides

---
+++ tictactoe.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(){
+system("clear");
+
+    printf("\n"); 
+    printf("--------Jeu du Tic-Ta-Toe------\n");
+
+//Créer la grille de jeu et l'initialiser avec des espaces vides
+
+char tableau [3][3];
+int columns; 
+int rows;
+       
+        for(columns=0; columns<3; columns++) 
+         
+	for(rows=0; rows<3; rows++) {
+        tableau[columns][rows]= '   ';
+
+    }
+printf("\n"); 
+
+
+
+
+
+
+exit(0);
+
+}
