35b13dc68de53a66de2f27451b7334750ce69595
Charbelakplogan2002 <acharbel02@gmail.com>
2025-10-19 19:38:44 +0000

    Ajout de la version finale du fichier main

--- main.c
+++ main.c
@@ -1,64 +1,64 @@
+#include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include "tictactoe.h"
 
+
 int main(){
+system("clear");    
+srand(time(NULL));  
+char tableau[3][3];
+
+//On initialise le tableau
+initialise(tableau);
 
 char computer = 'O' ;
 char player = 'X' ;
 char empty = ' ';
 
-system("clear");
 
-    printf("\n"); 
-    printf("--------Jeu du Tic-Tac-Toe------\n");
-int number;
-    printf("Choisissez où jouer: \n");
-    if (number>=9){
-        printf("Veuillez inscrire un numéro de grille valide");
-        return 1;
-    }
-    scanf(%d, &number);
-
-//Créer la grille de jeu et l'initialiser avec des espaces vides
-
-char tableau [3][3];
-int columns; 
-int rows;
-       
-    for(columns=0; columns<3; columns++) 
-         
-	for(rows=0; rows<3; rows++) {
-        tableau[columns][rows]= '   '; //Initilisation du tableau avec des espaces vides
-
-    }
 printf("\n"); 
-
-//Attribuer les colonnes et les rangees à des int
-number_attribuation()
+printf("--------Jeu du Tic-Tac-Toe------\n");
 
 // On affiche le tableau 
-boarddisplay(char tableau [3][3]);
+boarddisplay(tableau);    
+    
+ while (1) {
+player_plays(tableau);
+boarddisplay(tableau);
+if (win_condition(player, tableau)) {
+    printf("C'est gagné ! \n");
+    exit(0);
+}
 
-//Faire en sorte que l'ordinateur remplisse la premiere case disponible
-computer_plays()
+    computer_plays(tableau);
+    boarddisplay(tableau);
+    if (win_condition(computer, tableau))  {
+        printf("C'est perdu !\n");
+        exit(0);
+    }
+}
 
-//Faire en sorte que le joueur joue 
-player_plays()
-
-// Check si il y a un gagnant 
-win_condition()
-
-
-
-
-
-
+if (draw(tableau)) {
+    printf("C'est un match nul !\n");
+    exit(0);
+}
 
 
 
-
-
-exit(0);
+return(0);
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
